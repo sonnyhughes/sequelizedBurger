bbfae996c821db7d9a9b20d6bf81b663cf56038f
Sonny <richardhughesjr@gmail.com>
2018-03-14 19:36:22 -0500

    Adds sequelize and converts burgers_controller.js and burger.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,52 @@
+var express = require("express");
+
+var router = express.Router();
+var db = require("../models/");
+
+// get route -> index
+router.get("/", function (req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function (req, res) {
+  // express callback response by calling burger.selectAllBurger
+  db.Burger.findAll({}).then(function (data) {
+    // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
+    var hbsObject = {
+      burgers: data
+    };
+    res.render("index", hbsObject);
+  });
+});
+
+// post route -> back to index
+router.post("/burgers/create", function (req, res) {
+  // takes the request object using it as input for buger.addBurger
+  db.Burger.create({
+    "burger_name": req.body.burger_name
+  }).then(function (result) {
+    // wrapper for orm.js that using MySQL insert callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+// put route -> back to index
+router.put("/burgers/update/:id", function (req, res) {
+  db.Burger.update({
+    devoured: true
+  }, {
+    where: {
+      "id": req.params.id
+    }
+  }).then(function (result) {
+    // wrapper for orm.js that using MySQL update callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    // Send back response and let page reload from .done in Ajax
+    res.json("/");
+  });
+});
+
+module.exports = router;
